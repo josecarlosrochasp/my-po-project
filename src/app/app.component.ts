92f48fb5d18d29a52e79aee3ace5917112af7c7e
Jose Carlos <jose.darocha@totvs.com.br>
2023-12-04 22:05:33 -0300

    Criação do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { PoMenuItem, PoSelectOption } from '@po-ui/ng-components';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-po-project';
+export class AppComponent implements OnDestroy, OnInit {
+
+  stateOption: string = 'SAO PAULO';
+  state: any ;
+  stateOptions: Array<PoSelectOption> = [];
+  contact: any;
+
+  readonly menus: Array<PoMenuItem> = [
+    { label: 'Home', action: this.onClick.bind(this) }
+  ];
+
+  //private statesSubscription: Subscription;
+
+  private onClick() {
+    alert('Clicked in menu item')
+  }
+
+  ngOnInit() {
+    this.initialize();
+  }
+
+  ngOnDestroy(){
+    //this.statesSubscription?.unsubscribe();
+  }
+
+  initialize() {
+    this.state = '01'
+    this.stateOptions = [
+                          { label: 'SAO PAULO'     , value: '01' },
+                          { label: 'RIO DE JANEIRO', value: '02' },
+                          { label: 'MINAS GERAIS'  , value: '03' },
+                          { label: 'BAHIA'         , value: '04' }
+                        ];
+    return this.state = this.stateOptions[1].value;
+   // this.getStateByValue(this.state);
+   // console.log('state',this.state);
+   // console.log('state',this.stateOptions);
+   // this.stateOptions.values
+  }
+
+  EventSelecFilial(id: string): void {
+    console.log('EventSelecFilial',id);
+    this.getStateByValue(id);
+  }
+
+  getStateByValue(stateValue: string) {
+    const stateLabel = this.stateOptions.find(state => state.value === stateValue);
+    return stateLabel ? stateLabel.label : '';
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProFieldsModule, ProtheusLibCoreModule } from '@totvs/protheus-lib-core';
+import { FormsModule } from '@angular/forms';
+import { PoFieldModule, PoModule } from '@po-ui/ng-components';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { PoTemplatesModule } from '@po-ui/ng-templates';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProtheusLibCoreModule,
+    ProFieldsModule,
+    FormsModule,
+    PoModule,
+    HttpClientModule,
+    RouterModule.forRoot([]),
+    PoTemplatesModule,
+    PoFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
